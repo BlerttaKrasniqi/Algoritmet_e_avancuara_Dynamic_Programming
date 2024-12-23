066ef00b38dad17f5d9c1822d18ad9e6f9b0bf62
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-12-23 14:40:43 +0000

    Update Recursion_Memoization.java

--- Recursion_Memoization.java
+++ Recursion_Memoization.java
@@ -4,65 +4,58 @@ import java.util.Map;
 
 public class Recursion_Memoization {
 
-
-    public static int countWaysRecursive(int[] balls, int white, int black) {
-        return countWays(0, 0, balls, white, black, true, new HashMap<>());
-    }
-
-    private static int countWays(int index, int ballOffset, int[] balls, int white, int black, boolean isWhite, Map<String, Integer> memo) {
-
-        if (index >= balls.length && ballOffset == 0) {
+    private static int BackTrack(int[] row, int start, int white, int black, boolean WhiteSegment){
+        if(start==row.length){
             return 1;
         }
-        if (index >= balls.length) {
-            return 0;
-        }
-
-
-        String key = index + "_" + ballOffset + "_" + isWhite;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
-        int ways = 0;
-
-
-        int currentBallCount = balls[index];
-        int remainingBalls = currentBallCount - ballOffset;
-
-
-        if (isWhite) {
-            if (remainingBalls >= white) {
-                ways += countWays(index, ballOffset + white, balls, white, black, false, memo);
+        int count=0;
+        int whitecount=0;
+        int blackcount=0;
+        for(int i=start;i<row.length;i++){
+            if(row[i]==1){
+                whitecount++;
             }
-            if (remainingBalls == white) {
-                ways += countWays(index + 1, 0, balls, white, black, false, memo);
+            else{
+                blackcount++;
+            }
+            if(WhiteSegment && whitecount==white){
+                count+=BackTrack(row,i+1,white,black,false);
+            }
+            else if(!WhiteSegment && blackcount==black){
+                count+=BackTrack(row,i+1,white,black,true);
             }
         }
-
-
-        if (!isWhite) {
-            if (remainingBalls >= black) {
-                ways += countWays(index, ballOffset + black, balls, white, black, true, memo);
-            }
-            if (remainingBalls == black) {
-                ways += countWays(index + 1, 0, balls, white, black, true, memo);
-            }
-        }
-
-        
-        memo.put(key, ways);
-        return ways;
+        return count;
     }
 
-    public static void main(String[] args) {
+
+    public static int count_ways(int[] balls, int white, int black){
+        int n = Arrays.stream(balls).sum();
+        int[] row = new int[n];
+
+        int index = 0;
+        boolean WhiteBall = true;
+        for (int count:balls){
+            for(int j=0;j<count;j++){
+                row[index++]=WhiteBall ?1:0;
+
+            }
+            WhiteBall=!WhiteBall;
+        }
+
+        return BackTrack(row,0,white,black,true)+BackTrack(row,0,white,black,false);
+    }
+    public static void main(String[] args){
         int[] balls = {2, 2};
         int white = 1, black = 2;
-        System.out.println("Single Function Result: " + countWaysRecursive(balls, white, black));
+        System.out.println("Backtracking Result: " + count_ways(balls, white, black));
 
         balls = new int[]{2, 1, 3};
         white = 1;
         black = 1;
-        System.out.println("Single Function Result: " + countWaysRecursive(balls, white, black));
+        System.out.println("Backtracking Result: " + count_ways(balls, white, black));
+
     }
+
+
 }
