f6f1169f92a469c5e23a1bc8d5fb5f9cc831864b
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-12-26 23:19:16 +0000

    changes

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -2,72 +2,69 @@ import java.util.*;
 
 public class DynamicProgramming {
 
-    public static int countWaysRecursive(int[] balls, int white, int black) {
-
-        int n = Arrays.stream(balls).sum();
+    public static int countWaysDP(int[] balls, int white, int black) {
+        int n = Arrays.stream(balls).sum(); 
         int[] row = new int[n];
+
+
         int index = 0;
         boolean isWhite = true;
-
         for (int count : balls) {
-            for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
                 row[index++] = isWhite ? 1 : 0;
             }
             isWhite = !isWhite;
         }
 
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
 
-        return backtrack(row, 0, white, black);
+        for (int i = 1; i <= n; i++) {
+            int whiteCount = 0, blackCount = 0;
+            for (int j = i; j > 0; j--) {
+                if (row[j - 1] == 1) {
+                    whiteCount++;
+                } else {
+                    blackCount++;
+                }
+
+                if (whiteCount == white) {
+                    dp[i] += dp[j - 1];
+                }
+
+                if (blackCount == black) {
+                    dp[i] += dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n];
     }
 
-    private static int backtrack(int[] row, int start, int white, int black) {
-        if (start == row.length) {
-            return 1;
-        }
-
-        int totalWays = 0;
-
-
-        int whiteCount = 0;
-        for (int i = start; i < row.length; i++) {
-            if (row[i] == 1) {
-                whiteCount++;
-            }
-            if (whiteCount == white) {
-                totalWays += backtrack(row, i + 1, white, black);
-                break;
-            }
-        }
-
-
-        int blackCount = 0;
-        for (int i = start; i < row.length; i++) {
-            if (row[i] == 0) {
-                blackCount++;
-            }
-            if (blackCount == black) {
-                totalWays += backtrack(row, i + 1, white, black);
-                break;
-            }
-        }
-
-        return totalWays;
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] balls = {2, 2};
         int white = 1, black = 2;
-        System.out.println("Recursive Backtracking Result: " + countWaysRecursive(balls, white, black));
+        System.out.println("Dynamic Programming Result: " + countWaysDP(balls, white, black));
 
         balls = new int[]{2, 1, 3};
         white = 1;
         black = 1;
-        System.out.println("Recursive Backtracking Result: " + countWaysRecursive(balls, white, black));
+        System.out.println("Dynamic Programming Result: " + countWaysDP(balls, white, black));
 
-        balls = new int[]{1, 1, 1, 1, 1};
+
+
+        balls = new int[]{1,1,1,1,1};
         white = 1;
         black = 1;
-        System.out.println("Recursive Backtracking Result: " + countWaysRecursive(balls, white, black));
+        System.out.println("Dynamic Programming Result: " + countWaysDP(balls, white, black));
 
     }
 }
+
+
+
+
+
+
+
+
